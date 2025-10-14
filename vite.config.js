b8c08c09ee56aad2c3951ece2378d75d42fa8282
Hanlin Bai <hanlinbai@hotmail.com>
2025-10-14 21:39:57 +0100

    Updated deploying

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/EduAI/',
   plugins: [react()],
-  base: '/EduAI/', // only if deploying to a subdirectory like GitHub Pages
-});
+})
